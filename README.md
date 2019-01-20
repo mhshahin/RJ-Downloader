0425a64c553c747d23c99e4a3f8a8bb288bedc7f
Mohammad <mhshahin91@gmail.com>
2019-01-20 21:38:13 +0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# RJ-Downloader
+
+A simple script to download musics from Radio Javan website.
+Just pass the link of the intended music as the first argument, and voila!
+This script won't run under Windows, unless somehow you've managed to install "wget" on your Windows machine.
---
+++ rj_downloader.py
@@ -0,0 +1,23 @@
+import requests
+import json
+import re
+from subprocess import call
+
+def get_download_link(link):
+    media_type = re.split(r'/', link)[3]
+    file_name = re.split(r'/', link)[5]
+
+    session = requests.Session()
+    if media_type == "podcasts":
+        response = session.get("https://www.radiojavan.com/podcasts/podcast_host/?id=%s" %(file_name))
+        url = str(json.loads(response.text)['host']) + "/media/podcast/mp3-256/" + file_name + ".mp3"
+    elif media_type == "mp3s":
+        response = session.get("https://www.radiojavan.com/mp3s/mp3_host/?id=%s" %(file_name))
+        url = str(json.loads(response.text)['host']) + "/media/mp3/mp3-256/" + file_name + ".mp3"
+
+    return url
+
+if __name__ == "__main__":
+    link = input("Enter link: ")
+    download_link = get_download_link(link)
+    call(["wget", download_link])
