c0c692023a420fed9b42e58520c01025d23e6bd0
underline <gilas5000@gmail.com>
2020-03-30 00:20:06 +0430

    feat: download video

--- rj_downloader.py
+++ rj_downloader.py
@@ -14,10 +14,18 @@ def get_download_link(link):
     elif media_type == "mp3s":
         response = session.get("https://www.radiojavan.com/mp3s/mp3_host/?id=%s" %(file_name))
         url = str(json.loads(response.text)['host']) + "/media/mp3/" + file_name + ".mp3"
+    elif media_type == "videos":
+        response = session.get("https://www.radiojavan.com/videos/video_host/?id=%s" %(file_name))
+        url = str(json.loads(response.text)['host']) + "/media/music_video/hq/" + file_name + ".mp4"
+    else:
+        return None
 
     return url
 
 if __name__ == "__main__":
     link = input("Enter link: ")
     download_link = get_download_link(link)
-    call(["wget", download_link])
+    if download_link:
+        call(["wget", download_link])
+    else:
+        print('Sorry, unsupported link!')
