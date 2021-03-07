a76af20e6388f71a42f12135736527e2acaba652
Kasra Hassani <kasra.hassani@hotmail.com>
2021-03-07 11:48:28 +0330

    Modified rj_downloader.py
    feat options for both music and video

--- rj_downloader.py
+++ rj_downloader.py
@@ -1,44 +1,75 @@
-import logging
-
+#This is the main code that was written by Mohammad H. Shahin that I had found it on Github and changed it for my own usuage
+#You don't need to change anything in this code :)
+import re
 import requests
 import json
-import re
-from subprocess import call
-import validators
-
+import sys
+import os
 
 def get_download_link(link):
-    media_type = re.split(r"/", link)[3]
-    file_name = re.split(r"/", link)[5]
+    media_type = re.split(r'/', link)[3]
+    file_name = re.split(r'/', link)[5]
+    list=[]
+    feat=0
+    for i in range(0,len(file_name)):
+      if file_name[i]=="(":
+        feat=1
 
     session = requests.Session()
-    response = session.get(f"https://www.radiojavan.com/{media_type}/{media_type[:-1]}_host/?id={file_name}")
-    base_url = str(json.loads(response.text)["host"])
     if media_type == "podcasts":
-        return f"{base_url}/media/podcast/mp3-256/{file_name}.mp3"
-    elif media_type == "mp3s":
-        return f"{base_url}/media/mp3/{file_name}.mp3"
-    elif media_type == "videos":
-        return f"{base_url}/media/music_video/hq/{file_name}.mp4"
-    else:
-        return None
+        response = session.get("https://www.radiojavan.com/podcasts/podcast_host/?id=%s" %(file_name))
+        url = str(json.loads(response.text)['host']) + "/media/podcast/mp3-256/" + file_name + ".mp3"
+#        print (url)
+        os.system("wget -q " + url)
+
+    elif media_type == "mp3s" and feat==0:
+        response = session.get("https://www.radiojavan.com/mp3s/mp3_host/?id=%s" %(file_name))
+        url = str(json.loads(response.text)['host']) + "/media/mp3/" + file_name + ".mp3"
+#        print(url)
+        os.system("wget -q " + url)
+
+    elif media_type == "mp3s" and feat==1:
+        response = session.get("https://www.radiojavan.com/mp3s/mp3_host/?id=%s" %(file_name))
+        url = str(json.loads(response.text)['host']) + "/media/mp3/" + file_name + ".mp3"
+        for i in range(0,len(url)):
+            if url[i]=="(":
+               list.append("\\")
+               list.append(url[i])
+            elif url[i]==")":
+               list.append("\\")
+               list.append(url[i])
+            else:
+               list.append(url[i])
+
+        url=""
+        for i in range(0,len(list)):
+               url+=list[i]
+
+        os.system("wget -q " + url)
+    elif media_type == "videos" and feat==0:
+        response = session.get("https://www.radiojavan.com/videos/video_host/?id=%s" %(file_name))
+        url = str(json.loads(response.text)['host']) + "/media/music_video/hq/" + file_name + ".mp4"
+        os.system("wget -q " + url)
 
 
-if __name__ == "__main__":
-    while True:
-        media_link = input("Enter a link: ")
-        if not validators.url(media_link):
-            print("Please input a valid URL")
-            continue
-        break
+    elif media_type == "videos" and feat==1:
+        response = session.get("https://www.radiojavan.com/videos/video_host/?id=%s" %(file_name))
+        url = str(json.loads(response.text)['host']) + "/media/music_video/hq/" + file_name + ".mp4"
+        for i in range(0,len(url)):
+            if url[i]=="(":
+               list.append("\\")
+               list.append(url[i])
+            elif url[i]==")":
+               list.append("\\")
+               list.append(url[i])
+            else:
+               list.append(url[i])
 
-    download_link = None
-    try:
-        download_link = get_download_link(media_link)
-    except Exception as e:
-        logging.exception(e)
+        url=""
+        for i in range(0,len(list)):
+               url+=list[i]
 
-    if download_link:
-        call(["wget", download_link])
-    else:
-        print("Sorry, unsupported link!")
+        os.system("wget -q " + url)
+
+link = sys.argv[1]
+get_download_link(link)
